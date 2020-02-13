260c303b82207f86bf609bef7a5ea8d5602b3a4c
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-13 12:18:26 +0000

    Added company class. It implements parcelable interface.

---
+++ app/src/main/java/com/example/portfoliobalancer/Company.java
@@ -0,0 +1,32 @@
+package com.example.portfoliobalancer;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Company implements Parcelable {
+
+
+    protected Company(Parcel in) {
+    }
+
+    public static final Creator<Company> CREATOR = new Creator<Company>() {
+        @Override
+        public Company createFromParcel(Parcel in) {
+            return new Company(in);
+        }
+
+        @Override
+        public Company[] newArray(int size) {
+            return new Company[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+}
