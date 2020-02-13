730344a8577d4dc43270741cd3e95289a92ef6b0
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-13 17:10:26 +0000

    Added UserData Class (Implements Pareclable interface)

---
+++ app/src/main/java/com/example/portfoliobalancer/UserData.java
@@ -0,0 +1,94 @@
+package com.example.portfoliobalancer;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserData implements Parcelable
+{
+    //-----------------------------Instance variables-----------------------------
+
+    private List<Portfolio> portfolios = new ArrayList<Portfolio>();
+
+    //-----------------------------Getters/Setters-----------------------------
+
+
+    public List<Portfolio> getPortfolios() {
+        return portfolios;
+    }
+
+    public void setPortfolios(List<Portfolio> portfolios) {
+        this.portfolios = portfolios;
+    }
+
+    //-----------------------------Constructors-----------------------------
+
+
+    public UserData(List<Portfolio> portfolios)
+    {
+        this.portfolios = portfolios;
+    }
+
+    public UserData(UserData u)
+    {
+        this.portfolios = u.portfolios;
+    }
+
+    protected UserData(Parcel in)
+    {
+        this.portfolios = in.readArrayList(null);
+    }
+
+    //-----------------------------Implemented Parcelable Constructor/Methods-----------------------------
+
+    public static final Creator<UserData> CREATOR = new Creator<UserData>()
+    {
+        @Override
+        public UserData createFromParcel(Parcel in)
+        {
+            return new UserData(in);
+        }
+
+        @Override
+        public UserData[] newArray(int size)
+        {
+            return new UserData[size];
+        }
+    };
+
+    @Override
+    public int describeContents()
+    {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags)
+    {
+        dest.writeList(this.portfolios);
+    }
+
+    //-----------------------------Methods-----------------------------
+
+    public Portfolio addPortfolio()
+    {
+        return null;
+    }
+
+    public void removePortfolio(Portfolio p)
+    {
+
+    }
+
+    public List<Portfolio> loadUserData()
+    {
+        return null;
+    }
+
+    public void saveUserData(List<Portfolio> p)
+    {
+
+    }
+}
