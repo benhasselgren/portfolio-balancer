a10eee5c2ca7a33bf2c3ad90199db6412378a95e
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-14 13:24:34 +0000

    Added PortfolioDetails class

---
+++ app/src/main/java/com/example/portfoliobalancer/PortfolioDetails.java
@@ -0,0 +1,6 @@
+package com.example.portfoliobalancer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PortfolioDetails extends AppCompatActivity {
+}
--- app/src/main/java/com/example/portfoliobalancer/PortfoliosHolder.java
+++ app/src/main/java/com/example/portfoliobalancer/PortfoliosHolder.java
@@ -3,16 +3,45 @@ package com.example.portfoliobalancer;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PortfoliosHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    public PortfoliosHolder(@NonNull View itemView) {
+    private final TextView portfolioName;
+
+    private Portfolio portfolio;
+    private Context context;
+
+    public PortfoliosHolder(Context context, View itemView) {
         super(itemView);
+
+        this.context = context;
+
+        this.portfolioName = (TextView) itemView.findViewById(R.id.entry_location_name);
+
+        // Set the "onClick" listener of the holder
+        // here we use (this) because this class has a onClick function thanks
+        // to implementing the PlacesHolder class with the
+        // View.OnClickListener interface
+        itemView.setOnClickListener(this);
+    }
+
+    public void bindPortfolio(Portfolio portfolio) {
+        // Bind the data to the ViewHolder
+        this.portfolio = portfolio;
+        this.portfolioName.setText(portfolio.getName());
     }
 
     @Override
     public void onClick(View v) {
-
+        if (this.portfolio != null) {
+            Intent intent = new Intent(itemView.getContext(), LocationDetails.class);
+            intent.putExtra("portfolio", this.portfolio);
+            itemView.getContext().startActivity(intent);
+        }
     }
 }
