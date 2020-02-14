125829036bfe9586345c7a3154611373b42560c0
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-14 13:26:12 +0000

    Added portfoliop details xaml file which will display the data the portfolio data

--- app/src/main/java/com/example/portfoliobalancer/PortfolioDetails.java
+++ app/src/main/java/com/example/portfoliobalancer/PortfolioDetails.java
@@ -1,6 +1,32 @@
 package com.example.portfoliobalancer;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PortfolioDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_details);
+
+        Place place =
+                (Place) getIntent().getParcelableExtra("place");
+
+        if (place != null) {
+            TextView locationTitle =
+                    (TextView) findViewById(R.id.location_info_title);
+            ImageView locationImage =
+                    (ImageView) findViewById(R.id.location_info_image);
+            TextView locationInfo =
+                    (TextView) findViewById(R.id.location_info_body);
+
+            locationTitle.setText(place.getNameId());
+            locationImage.setImageResource(place.getIconId());
+            locationInfo.setText(place.getInfo());
+        }
+    }
 }
