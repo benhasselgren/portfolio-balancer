0a9f00b39779a45e48fd7bdbeb49bfe7af7a7c50
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-19 11:30:19 +0000

    Added add portfolio activity one xaml

--- app/src/main/java/com/example/portfoliobalancer/AddPortfolioActivityOne.java
+++ app/src/main/java/com/example/portfoliobalancer/AddPortfolioActivityOne.java
@@ -1,7 +1,12 @@
 package com.example.portfoliobalancer;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class AddPortfolioActivityOne extends AppCompatActivity {
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
