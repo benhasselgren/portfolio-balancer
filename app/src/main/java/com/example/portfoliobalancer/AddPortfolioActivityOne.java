5db095924331b7496b0427170c6291aadba47089
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-19 12:48:42 +0000

    Added comments and fixed indentation

--- app/src/main/java/com/example/portfoliobalancer/AddPortfolioActivityOne.java
+++ app/src/main/java/com/example/portfoliobalancer/AddPortfolioActivityOne.java
@@ -17,7 +17,7 @@ public class AddPortfolioActivityOne extends AppCompatActivity {
         setContentView(R.layout.add_portfolio_activity_one);
 
         //Assigns the name field to a variable and adds validation
-        EditText name = (EditText) findViewById(R.id.add_portfolio_name_input);
+        final EditText name = (EditText) findViewById(R.id.add_portfolio_name_input);
 
         name.setFilters(new InputFilter[] {
                 new InputFilter.LengthFilter(15)
@@ -30,12 +30,8 @@ public class AddPortfolioActivityOne extends AppCompatActivity {
                 new InputFilter.LengthFilter(30)
         });
 
-        //Assigns the amount field to a variable and adds validation
+        //Assigns the amount field to a variable
         final EditText amount = (EditText) findViewById(R.id.add_portfolio_amount_input);
-
-        amount.setFilters(new InputFilter[] {
-                new InputFilterMinMax("100", "25000")
-        });
 
         //Assigns the button to a variable
         Button nextButton = (Button) findViewById(R.id.add_portfolio_activity_one_btn);
@@ -44,7 +40,45 @@ public class AddPortfolioActivityOne extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                Toast.makeText(getBaseContext(), "Hurray!", Toast.LENGTH_SHORT).show();
+                //Convert amount to string, then int
+                String amountString = name.getText().toString();
+                int amountInt = Integer.parseInt(amountString);
+                //Convert namd and description to strings
+                String nameString = name.getText().toString();
+                String descriptionString = name.getText().toString();
+
+                //Set both validators to false intially
+                boolean amountValid = false;
+                boolean textValid = false;
+
+                //If amount is within range then set amountValid to true
+                if (amountInt >= 100 && amountInt <= 25000)
+                {
+                    amountValid = true;
+
+                    //If name and descrption both have values then set textValid to true
+                    if(nameString.length() > 0 && descriptionString.length() > 0)
+                    {
+                        textValid = true;
+                    }
+                }
+
+                //Display appropriate messages if everything is valid or not valid
+                if(amountValid)
+                {
+                    if(textValid)
+                    {
+                        Toast.makeText(getBaseContext(), "Hurray!", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(getBaseContext(), "You need to fill in all the fields", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    Toast.makeText(getBaseContext(), "Amount needs to be between £100 and £25,000", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
