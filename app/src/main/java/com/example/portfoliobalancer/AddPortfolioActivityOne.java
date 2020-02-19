7d727a235b5c956a899ad0de7439bd9df8b397d2
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-19 13:06:42 +0000

    Changed button click method to use a try catch format and also created a checkvalidation method that throws exceptions if there's an error

--- app/src/main/java/com/example/portfoliobalancer/AddPortfolioActivityOne.java
+++ app/src/main/java/com/example/portfoliobalancer/AddPortfolioActivityOne.java
@@ -12,26 +12,30 @@ import com.example.portfoliobalancer.InputFilterMinMax;
 
 public class AddPortfolioActivityOne extends AppCompatActivity {
 
+    private EditText name;
+    private EditText description;
+    private EditText amount;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_portfolio_activity_one);
 
         //Assigns the name field to a variable and adds validation
-        final EditText name = (EditText) findViewById(R.id.add_portfolio_name_input);
+        name = (EditText) findViewById(R.id.add_portfolio_name_input);
 
         name.setFilters(new InputFilter[] {
                 new InputFilter.LengthFilter(15)
         });
 
         //Assigns the description field to a variable and adds validation
-        final EditText description = (EditText) findViewById(R.id.add_portfolio_description_input);
+        description = (EditText) findViewById(R.id.add_portfolio_description_input);
 
         description.setFilters(new InputFilter[] {
                 new InputFilter.LengthFilter(30)
         });
 
         //Assigns the amount field to a variable
-        final EditText amount = (EditText) findViewById(R.id.add_portfolio_amount_input);
+        amount = (EditText) findViewById(R.id.add_portfolio_amount_input);
 
         //Assigns the button to a variable
         Button nextButton = (Button) findViewById(R.id.add_portfolio_activity_one_btn);
@@ -40,28 +44,16 @@ public class AddPortfolioActivityOne extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                //Convert amount to string, then int
-                String amountString = name.getText().toString();
-                int amountInt = Integer.parseInt(amountString);
-                //Convert namd and description to strings
-                String nameString = name.getText().toString();
-                String descriptionString = name.getText().toString();
-
-                //Set both validators to false intially
-                boolean amountValid = false;
-                boolean textValid = false;
-
-                //If amount is within range then set amountValid to true
-                if (amountInt >= 100 && amountInt <= 25000)
+                try
                 {
-                    amountValid = true;
-
-                    //If name and descrption both have values then set textValid to true
-                    if(nameString.length() > 0 && descriptionString.length() > 0)
-                    {
-                        textValid = true;
-                    }
+                    checkValidation();
                 }
+                catch (IllegalArgumentException ex)
+                {
+
+                }
+
+
 
                 //Display appropriate messages if everything is valid or not valid
                 if(amountValid)
@@ -81,5 +73,34 @@ public class AddPortfolioActivityOne extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void checkValidation()
+    {
+        //Convert amount to string, then int
+        String amountString = name.getText().toString();
+        int amountInt = Integer.parseInt(amountString);
+        //Convert name and description to strings
+        String nameString = name.getText().toString();
+        String descriptionString = name.getText().toString();
+
+        if (nameString.length() <= 0)
+        {
+            throw new IllegalArgumentException("Name field cannot be empty");
+        }
+        else if(descriptionString.length() <=0)
+        {
+            throw new IllegalArgumentException("Description field cannot be empty");
+        }
+        else if(amountString.length() <=0)
+        {
+            throw new IllegalArgumentException("Amount field cannot be empty");
+        }
+        else {
+            if (amountInt < 100 || amountInt > 25000)
+            {
+                throw new IllegalArgumentException("Amount has to be between £100-£25,000");
+            }
+        }
     }
 }
