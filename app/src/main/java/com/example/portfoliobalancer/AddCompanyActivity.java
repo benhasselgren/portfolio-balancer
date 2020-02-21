9b028f1d59051135dc51a45f4e388201bc142232
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-21 13:53:30 +0000

    Added search bar to xaml file (addCompany)

--- app/src/main/java/com/example/portfoliobalancer/AddCompanyActivity.java
+++ app/src/main/java/com/example/portfoliobalancer/AddCompanyActivity.java
@@ -3,11 +3,16 @@ package com.example.portfoliobalancer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.portfoliobalancer.classes.Portfolio;
+
 public class AddCompanyActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_company);
+
+        //Assignt the intent parcelable extra to a variable
+        Portfolio portfolio = (Portfolio) getIntent().getParcelableExtra("portfolio");
     }
 
 }
