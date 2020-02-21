d95ad8388a0048e9b6ddf8af524d343222a847ab
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-21 14:28:21 +0000

    Added the addcompany activity to the manifest. Activity now appears

--- app/src/main/java/com/example/portfoliobalancer/AddCompanyActivity.java
+++ app/src/main/java/com/example/portfoliobalancer/AddCompanyActivity.java
@@ -3,11 +3,15 @@ package com.example.portfoliobalancer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.portfoliobalancer.classes.Company;
 import com.example.portfoliobalancer.classes.Portfolio;
+
+import java.util.ArrayList;
 
 public class AddCompanyActivity extends AppCompatActivity {
 
     private Portfolio portfolio;
+    private ArrayList<Company> availableCompanies;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +19,21 @@ public class AddCompanyActivity extends AppCompatActivity {
 
         //Assign the intent parcelable extra to a variable
         portfolio = (Portfolio) getIntent().getParcelableExtra("portfolio");
+
+        /*
+        //Get the companies from the string array and add them to the companies strings array list
+        String[] strings = getResources().getStringArray(R.array.companies);
+
+        for(String s : strings)
+        {
+
+            String[] result = s.split(",");
+            String code = result[0];
+            String
+        }
+
+
+         */
     }
 
 }
