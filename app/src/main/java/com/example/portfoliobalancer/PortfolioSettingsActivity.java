b7fd79534aa8d72143f24cea172be619e4018dae
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-22 12:21:47 +0000

    Added portfolio settings activity and corresponding xaml file, containing a recycler view

---
+++ app/src/main/java/com/example/portfoliobalancer/PortfolioSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.portfoliobalancer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PortfolioSettingsActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portfolio_settings);
+    }
+
+    }
