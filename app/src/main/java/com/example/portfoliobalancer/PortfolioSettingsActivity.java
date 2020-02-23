2282359c194bd1549685209885c32df7f57b75cb
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-23 12:18:21 +0000

    Set up on click button that checks to see if target percetages total 100%. If they don't show error message, if they do, create portfolio and return user to main screen

--- app/src/main/java/com/example/portfoliobalancer/PortfolioSettingsActivity.java
+++ app/src/main/java/com/example/portfoliobalancer/PortfolioSettingsActivity.java
@@ -1,16 +1,24 @@
 package com.example.portfoliobalancer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.portfoliobalancer.classes.Portfolio;
 
+//######################-----------------------------PortfoliosSettingsActivityClass-----------------------------######################
+//XML file: activity_portfolio_settings.xml
+//Portfolio settings page of app. It's where the user can edit their portfolio details and change the target percentages.
+//Also where the user is directed when creating a new portfolio
 public class PortfolioSettingsActivity extends AppCompatActivity  {
 
     //-----------------------------Variables/Views-----------------------------
@@ -22,6 +30,7 @@ public class PortfolioSettingsActivity extends AppCompatActivity  {
     private EditText name;
     private EditText description;
     private EditText amount;
+    private Button rebalance_create_btn;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,9 +45,10 @@ public class PortfolioSettingsActivity extends AppCompatActivity  {
         //Add views
         portfoliosTargetPercentagesListView = (RecyclerView)findViewById(R.id.portfolios_target_percentages_list);
         totalPercentage = (TextView)findViewById(R.id.portfolio_target_percentage_total);
-        name = (EditText) findViewById(R.id.portfolio_name_input);
-        description = (EditText) findViewById(R.id.portfolio_description_input);
-        amount = (EditText) findViewById(R.id.portfolio_amount_input);
+        name = (EditText)findViewById(R.id.portfolio_name_input);
+        description = (EditText)findViewById(R.id.portfolio_description_input);
+        amount = (EditText)findViewById(R.id.portfolio_amount_input);
+        rebalance_create_btn = (Button) findViewById(R.id.rebalance_create__btn);
 
         //If the recyclerview doesn't change size, we can set this true and
         portfoliosTargetPercentagesListView.setHasFixedSize(true);
@@ -57,6 +67,30 @@ public class PortfolioSettingsActivity extends AppCompatActivity  {
         description.setText(portfolio.getDescription());
         amount.setText(String.format("£%.2f", portfolio.getCurrentPrice()));
 
-        totalPercentage.setText(String.format("%s", portfolio.getTotalPercentage()));
+        //Set portfolio target percentage total
+        totalPercentage.setText(String.format("%s%%", portfolio.getTotalPercentage()));
+
+        //Triggers if rebalance/create button is clicked
+        rebalance_create_btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                //Set the text to show
+                totalPercentage.setText(String.format("%s%%", portfolio.getTotalPercentage()));
+
+                //If the total percentage is = 100 then save target percentages and go back to main activity
+                if(portfolio.getTotalPercentage() == 100)
+                {
+                    Intent intent = new Intent(PortfolioSettingsActivity.this, MainActivity.class);
+                    intent.putExtra("portfolio", portfolio);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    //Show error message if total pecentage does not equal 100%
+                    Toast.makeText(getBaseContext(), "The total percentage must equal 100%", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
