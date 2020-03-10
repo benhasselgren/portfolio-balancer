a0b0885c8008a6b2f129258d4cd156ca80ebfa4a
Ben Hasselgren <ben.nadalito@gmail.com>
2020-03-10 14:42:10 +0000

    Added class that handles on swipe callbacks

---
+++ app/src/main/java/com/example/portfoliobalancer/companies_prices_activity/SwipeToDeleteCallback.java
@@ -0,0 +1,28 @@
+package com.example.portfoliobalancer.companies_prices_activity;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+import com.example.portfoliobalancer.companies_prices_activity.CompaniesPricesAdapter;
+
+public class SwipeToDeleteCallback extends ItemTouchHelper.SimpleCallback {
+
+    private CompaniesPricesAdapter mAdapter;
+
+    public SwipeToDeleteCallback(CompaniesPricesAdapter adapter) {
+        super(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+        mAdapter = adapter;
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+        int position = viewHolder.getAdapterPosition();
+        mAdapter.deleteItem(position);
+    }
+}
